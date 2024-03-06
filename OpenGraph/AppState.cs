0c6f3756b0873dee3012b3d53d4fc90b386d81cc
himawan <susantohimawan@gmail.com>
2024-03-06 09:29:46 +0700

    Add project files.

---
+++ OpenGraph/AppState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenGraph
+{
+    public class AppState
+    {
+        public MetaTags MetaTags { get; private set; } = new MetaTags();
+
+        public event Action? OnChange;
+
+        public void SetMetaTags(MetaTags metatags)
+        {
+            MetaTags = metatags;
+            NotifyStateChanged();
+        }
+
+        public void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
---
+++ OpenGraph/MetaTags.cs
@@ -0,0 +1,33 @@
+﻿namespace OpenGraph
+{
+    public class MetaTags
+    {
+        public string Title { get; set; } = "";
+
+        public string Type { get; set; } = "";
+
+        public string Url { get; set; } = "";
+
+        public string Description { get; set; } = "";
+
+        public string Image { get; set; } = "";
+
+        public string Imagesecureurl { get; set; } = "";
+        
+        public string Imagewidth { get; set; } = "";
+
+        public string Imageheight { get; set; } = "";
+
+        public string Imagetype { get; set; } = "";
+
+        public string Imagealt { get; set; } = "";
+
+        // Add other relevant properties as needed
+    }
+
+}
+
+//og: title - The title of your object as it should appear within the graph, e.g., "The Rock".
+//og:type - The type of your object, e.g., "video.movie". Depending on the type you specify, other properties may also be required.
+//og:image - An image URL which should represent your object within the graph.
+//og:url - The canonical URL of your object that will be used as its permanent ID in the graph, e.g., "https://www.imdb.com/title/tt0117500/".
